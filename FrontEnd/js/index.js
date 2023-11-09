fbd800f7098d2d35b56a980250ac7d88a0c1e9e3
Nicolas Lepaire <lepaire.nico@gmail.com>
2023-11-09 10:49:58 +0100

    Fetch & innerHTML

--- FrontEnd/js/index.js
+++ FrontEnd/js/index.js
@@ -1,1 +1,30 @@
-console.log("Hello World");
+const url = "http://localhost:5678/api/works";
+
+// fetch(url)
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((works) => {
+//     console.log(works);
+//   });
+
+fetch(url)
+  .then((response) => response.json())
+  .then((works) => {
+    console.log(works);
+
+    let gallery = document.getElementById("gallery");
+
+    let figure = "";
+    for (const work of works) {
+      console.log(work.title);
+      console.log(work.imageUrl);
+      figure += `<figure>
+      <img src="${work.imageUrl}" alt="${work.title}" />
+      <figcaption>${work.title}</figcaption>
+      </figure>`;
+    }
+
+    console.log(figure);
+    gallery.innerHTML = figure;
+  });
